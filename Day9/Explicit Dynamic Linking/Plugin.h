9f0c1679c4081182189d7324b84094d68b4807e8
Hiya Gurbani <hiya.gurbani@intimetec.com>
2026-01-21 16:01:47 +0530

    Added Explicit Dynamic Linking Code and Extern Usecase

---
+++ Day9/Explicit Dynamic Linking/Plugin.h
@@ -0,0 +1,21 @@
+#ifndef PLUGIN_H
+#define PLUGIN_H
+
+extern "C" int multiply(int a, int b);
+
+#endif
+
+// #ifndef PLUGIN_H
+// #define PLUGIN_H
+
+// #ifdef __cplusplus
+// extern "C" {
+// #endif
+
+// int multiply(int, int);
+
+// #ifdef __cplusplus
+// }
+// #endif
+
+// #endif
