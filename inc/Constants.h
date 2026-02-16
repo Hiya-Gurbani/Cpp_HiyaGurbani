20ffd91efbff03460f93b0f6bbd70e08c9433e51
Hiya Gurbani <hiya.gurbani@intimetec.com>
2026-02-16 15:09:09 +0530

    Updated customer login and switched to namespaces for constants and logger

--- inc/Constants.h
+++ inc/Constants.h
@@ -3,8 +3,8 @@
 
 #include <string>
 
-class Constants {
-public:
+namespace Constants {
+
     enum UserRole {
         ADMIN,
         CUSTOMER
@@ -25,41 +25,47 @@ public:
         PASSWORD
     };
 
-    static const long INITIAL_TRANSACTION_ID = 1;
-    static const long INITIAL_ACCOUNT_NUMBER = 10000000;
+    enum class LoginResult {
+        SUCCESS,
+        FAILED,
+        ACCOUNT_NOT_FOUND
+    };
 
-    static const int PIN_LENGTH = 4;
-    static const int MIN_PIN_VALUE = 1000;
-    static const int MAX_PIN_VALUE = 9999;
+    inline constexpr long INITIAL_TRANSACTION_ID = 1;
+    inline constexpr long INITIAL_ACCOUNT_NUMBER = 10000000;
 
-    static const int ACCOUNT_NUMBER_LENGTH = 8;
-    static const int PHONE_LENGTH = 10;
-    static const int MAX_LOGIN_ATTEMPTS = 3;
-    static const int MIN_NAME_LENGTH = 3;
-    static const int MIN_PASSWORD_LENGTH = 6;
-    static const int MINI_STATEMENT_MAX_TRANSACTIONS = 5;
+    inline constexpr int PIN_LENGTH = 4;
+    inline constexpr int MIN_PIN_VALUE = 1000;
+    inline constexpr int MAX_PIN_VALUE = 9999;
 
-    static const int MIN_TRANSACTION_AMOUNT = 100;
-    static const int MAX_TRANSACTION_AMOUNT = 10000;
+    inline constexpr int ACCOUNT_NUMBER_LENGTH = 8;
+    inline constexpr int PHONE_LENGTH = 10;
+    inline constexpr int MAX_LOGIN_ATTEMPTS = 3;
+    inline constexpr int MIN_NAME_LENGTH = 3;
+    inline constexpr int MIN_PASSWORD_LENGTH = 6;
+    inline constexpr int MINI_STATEMENT_MAX_TRANSACTIONS = 5;
 
-    static const int ID_WIDTH = 5;
-    static const int TYPE_WIDTH = 12;
-    static const int AMOUNT_WIDTH = 10;
-    static const int POSTBALANCE_WIDTH = 15;
-    static const int DECIMAL_PRECISION = 2;
+    inline constexpr int MIN_TRANSACTION_AMOUNT = 100;
+    inline constexpr int MAX_TRANSACTION_AMOUNT = 10000;
 
-    static const char NEW_LINE = '\n';
-    static const char AT_THE_RATE = '@';
-    static const char FULL_STOP = '.';
-    static const char WHITESPACE = ' ';
-    static const char HYPHEN = '-';
-    static const char TAB = '\t';
+    inline constexpr int ID_WIDTH = 5;
+    inline constexpr int TYPE_WIDTH = 12;
+    inline constexpr int AMOUNT_WIDTH = 10;
+    inline constexpr int POSTBALANCE_WIDTH = 15;
+    inline constexpr int DECIMAL_PRECISION = 2;
 
-    inline static const std::string EMPTY_STRING = "";
-    inline static const std::string ADMIN_NAME = "Hiya";
-    inline static const std::string ADMIN_USERNAME = "hiya";
-    inline static const std::string ADMIN_PASSWORD = "system"; 
-    inline static const std::string WHITESPACE_CHARS = " \t\n\r";
+    inline constexpr char NEW_LINE = '\n';
+    inline constexpr char AT_THE_RATE = '@';
+    inline constexpr char FULL_STOP = '.';
+    inline constexpr char WHITESPACE = ' ';
+    inline constexpr char HYPHEN = '-';
+    inline constexpr char TAB = '\t';
+
+    inline const std::string EMPTY_STRING = "";
+    inline const std::string ADMIN_NAME = "Hiya";
+    inline const std::string ADMIN_USERNAME = "hiya";
+    inline const std::string ADMIN_PASSWORD = "system"; 
+    inline const std::string WHITESPACE_CHARS = " \t\n\r";
 };
 
 #endif
--- readme.md
+++ readme.md
@@ -9,15 +9,7 @@ mkdir bin
 2. **Compile the source files into position-independent code (`.o` files):**
 
 ```bash
-g++ -I./inc -fPIC -c src/Account.cpp -o bin/Account.o
-g++ -I./inc -fPIC -c src/AdminController.cpp -o bin/AdminController.o
-g++ -I./inc -fPIC -c src/Bank.cpp -o bin/Bank.o
-g++ -I./inc -fPIC -c src/Customer.cpp -o bin/Customer.o
-g++ -I./inc -fPIC -c src/CustomerController.cpp -o bin/CustomerController.o
-g++ -I./inc -fPIC -c src/Display.cpp -o bin/Display.o
-g++ -I./inc -fPIC -c src/InputHandler.cpp -o bin/InputHandler.o
-g++ -I./inc -fPIC -c src/Transaction.cpp -o bin/Transaction.o
-g++ -I./inc -fPIC -c src/Validator.cpp -o bin/Validator.o
+cd bin && g++ -I../inc -fPIC -c ../src/*.cpp && cd ..
 ```
 
 3. **Create the shared library:**
@@ -29,12 +21,12 @@ g++ -shared -o libbank.so bin/*.o
 4. **Compile the main program and link it with the shared library:**
 
 ```bash
-g++ -I./inc main/main.cpp -L. -lbank -Wl,-rpath=. -o main.out
+g++ -I./inc main/main.cpp -L. -lbank -Wl,-rpath=. -o BankAppSimulator.out
 ```
 
 
 5. **Run the program:**
 
 ```bash
-./main.out
+./BankAppSimulator.out
 ```
