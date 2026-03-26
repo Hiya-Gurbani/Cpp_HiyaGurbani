318e02cc26a40d1a815db25a7f82f9aa2a9901fc
Hiya Gurbani <hiya.gurbani@intimetec.com>
2026-03-26 09:35:06 +0530

    Initial Commit of Assignment1 and Removed unnecessary folders

--- Week 2/Explicit Dynamic Linking/inc/Subtraction.h
+++
@@ -1,6 +0,0 @@
-#ifndef SUBTRACTION_H
-#define SUBTRACTION_H
-
-extern "C" double subtractOperands(double operand1, double operand2);
-
-#endif
--- Week 2/Explicit Dynamic Linking/src/Addition.cpp
+++
@@ -1,5 +0,0 @@
-#include "Addition.h"
-
-double addOperands(double operand1, double operand2) {
-    return operand1 + operand2;
-}
